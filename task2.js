f5ed151421ca0e4e7ecec3f3390989df6f31c24d
caradguadarrama <caradguadarrama@gmai.com>
2026-01-14 21:20:59 -0600

    subiendo actividades

--- task2.js
+++ task2.js
@@ -1,2 +1,23 @@
 // Task 2: listUsers()
+import { getServerURL } from "./task1.js";
+
+export async function listUsers() {
+    
+    const url = getServerURL(); 
+
+    try {
+        const response = await fetch(url);
+
+        
+        if (!response.ok) {
+            throw new Error(`Error en el servidor: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result); 
+        
+    } catch (error) {
+        console.error("Hubo un problema al obtener los usuarios:", error.message);
+    }
+}
 
--- task3.js
+++ task3.js
@@ -1,2 +1,24 @@
-// Task 3: addUser(first_name, last_name, email)
+ addUser(first_name, last_name, email)
+import { listUsers } from "./task2.js";
+import { getServerURL } from "./task1.js";
 
+export async function addUser(first_name, last_name, email) {
+    const users = await listUsers();
+    const maxId = Math.max(...users.map(user => user.id || 0));
+    const newId = (maxId + 1).toString();
+
+    await fetch(getServerURL() + "/users", {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                id: newId,
+                first_name: first_name,
+                last_name: last_name,
+                email: email
+            }
+        ),
+    })
+}
